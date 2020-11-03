cce0d414fe13013db1b4d710c91ebfff54db770e
Yurii Horchuk <yurii.horchuk@existek.com>
2020-11-03 10:45:49 +0200

    removed static from server

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,9 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { mySqlConfig } from './common/config/database.config';
 import { AuthModule } from './auth/auth.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import {join} from 'path';
+
 @Module({
   imports: [
-      ServeStaticModule.forRoot({
-          rootPath: join(__dirname, '..', 'client', 'build'),
-          exclude: ['/api*'],
-      }),
       SequelizeModule.forRoot(mySqlConfig),
       AuthModule,
   ],
